0da1dc8885c027fe594ea31d7a9038c9952e6f51
Ололо Евгений <vetero4ekcs@mail.ru>
2021-05-11 17:37:04 +0300

    fix_r

--- modules/queue/test/test_queue_app.cpp
+++ modules/queue/test/test_queue_app.cpp
@@ -13,123 +13,147 @@ TEST(test_queue_app, can_create) {
 }
 
 TEST(test_queue_app, empty_args) {
-    const int argc = 1;
     std::vector<const char*> argv = { "app"};
+    const int argc = argv.size();
     QueueApp app;
 
     ASSERT_NO_THROW(app(argc, argv.data()));
 }
 
 TEST(test_queue_app, invalid_args) {
-    const int argc = 2;
     std::vector<const char*> argv = { "app", "pepega" };
+    const int argc = argv.size();
     QueueApp app;
 
     std::string res = app(argc, argv.data());
-    std::string correct = "invalid arg";
+    const std::string correct = "invalid arg";
 
     ASSERT_EQ(correct, res);
 }
 
 TEST(test_queue_app, append) {
-    const int argc = 4;
     std::vector<const char*> argv = { "app", "append", "11.5", "get_first" };
+    const int argc = argv.size();
     QueueApp app;
 
     double res = stod(app(argc, argv.data()));
-    double correct = stod(std::string("11.5"));
+    const double correct = 11.5;
 
     ASSERT_EQ(correct, res);
 }
 
 TEST(test_queue_app, append_invalid) {
-    const int argc = 3;
     std::vector<const char*> argv = { "app", "append", "pepega" };
+    const int argc = argv.size();
     QueueApp app;
 
     std::string res = app(argc, argv.data());
-    std::string correct = "Wrong number format!";
+    const std::string correct = "Wrong number format!";
 
     ASSERT_EQ(correct, res);
 }
 
 TEST(test_queue_app, is_empty_true) {
-    const int argc = 2;
     std::vector<const char*> argv = { "app", "is_empty" };
+    const int argc = argv.size();
     QueueApp app;
 
     std::string res = app(argc, argv.data());
-    std::string correct = "1\n";
+    const std::string correct = "1\n";
 
     ASSERT_EQ(correct, res);
 }
 
 TEST(test_queue_app, is_empty_false) {
-    const int argc = 4;
     std::vector<const char*> argv = { "app", "append", "55", "is_empty" };
+    const int argc = argv.size();
     QueueApp app;
 
     std::string res = app(argc, argv.data());
-    std::string correct = "0\n";
+    const std::string correct = "0\n";
 
     ASSERT_EQ(correct, res);
 }
 
 TEST(test_queue_app, get_first) {
-    const int argc = 4;
     std::vector<const char*> argv = { "app", "append", "55.1", "get_first" };
+    const int argc = argv.size();
     QueueApp app;
 
     double res = stod(app(argc, argv.data()));
-    double correct = stod(std::string("55.1"));
+    const double correct = 55.1;
 
     ASSERT_EQ(correct, res);
 }
 
 TEST(test_queue_app, rm_first) {
-    const int argc = 7;
     std::vector<const char*> argv = { "app", "append", "55.1", "append", "66.2",
                                     "rm_first", "get_first" };
+    const int argc = argv.size();
     QueueApp app;
 
     double res = stod(app(argc, argv.data()));
-    double correct = stod(std::string("66.2"));
+    const double correct = 66.2;
 
     ASSERT_EQ(correct, res);
 }
 
 TEST(test_queue_app, get_last) {
-    const int argc = 6;
     std::vector<const char*> argv = { "app", "append", "55.1",
                                 "append", "66.2", "get_last" };
+    const int argc = argv.size();
     QueueApp app;
 
     double res = stod(app(argc, argv.data()));
-    double correct = stod(std::string("66.2"));
+    const double correct = 66.2;
 
     ASSERT_EQ(correct, res);
 }
 
 TEST(test_queue_app, get_size) {
-    const int argc = 6;
     std::vector<const char*> argv = { "app", "append", "55.1",
                                 "append", "66.2", "get_size" };
+    const int argc = argv.size();
     QueueApp app;
 
     int res = stoi(app(argc, argv.data()));
-    int correct = 2;
+    const int correct = 2;
 
     ASSERT_EQ(correct, res);
 }
 
 TEST(test_queue_app, get_all) {
-    const int argc = 6;
     std::vector<const char*> argv = { "app", "append", "55.1", "append",
                                                     "66.2", "get_all" };
+    const int argc = argv.size();
     QueueApp app;
 
     std::string res = app(argc, argv.data());
-    std::string correct = "55.100000 66.200000 \n";  // ?
+    const std::string correct = "55.100000 66.200000 \n";
+
+    ASSERT_EQ(correct, res);
+}
+
+TEST(test_queue_app, append_after_rm) {
+    std::vector<const char*> argv = { "app", "append", "11", "append", "22",
+                        "rm_first", "append", "33", "append", "44", "get_last"};
+    const int argc = argv.size();
+    QueueApp app;
+
+    double res = stod(app(argc, argv.data()));
+    const double correct = 44.0;
+
+    ASSERT_EQ(correct, res);
+}
+
+TEST(test_queue_app, rm_all) {
+    std::vector<const char*> argv = { "app", "append", "1", "append", "2",
+                                        "rm_first", "rm_first", "get_size"};
+    const int argc = argv.size();
+    QueueApp app;
+
+    int res = stoi(app(argc, argv.data()));
+    int correct = 0;
 
     ASSERT_EQ(correct, res);
 }
